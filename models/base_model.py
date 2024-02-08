06b2c429bc35b8b3d9a6bd48505036c5e91d607e
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-07 17:34:47 -0800

    update to include storage module

--- models/base_model.py
+++ models/base_model.py
@@ -2,26 +2,34 @@
 
 from datetime import datetime
 import uuid
-
+from models import storage
 
 class BaseModel:
     """
     Class that defines all common methods/attributes for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize Basemodel instance with unique id and current datetime.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """
         Update update_at with current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
