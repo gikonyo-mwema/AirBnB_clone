0f3051e3acd4f7295a7c3787d479c85215edd875
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-07 17:36:22 -0800

    serializes instances to a JSON file and deserializes JSON file to instances

---
+++ models/engine/file_storage.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    """
+    Class that serializes instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id
+        """
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (path: __file_path)
+        """
+        with open(self.__file_path, 'w') as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                self.__objects = {k: BaseModel(**v) for k, v in json.load(f).items()}
+        except FileNotFoundError:
+            pass
