c5564f05aa1b6babebd02a5d9148d013c5c171b1
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-08 08:20:18 -0800

    pyc file and others

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 import uuid
-from models import storage
+
 
 class BaseModel:
     """
@@ -22,13 +22,27 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
+            self.register()
+
+    def register(self):
+        """
+        Register the instance in storage
+        """
+        from models import storage
+        storage.new(self)
 
     def save(self):
         """
         Update update_at with current datetime
         """
         self.updated_at = datetime.now()
+        self.persist()
+
+    def persist(self):
+        """
+        Persist the instance in storage
+        """
+        from models import storage
         storage.save()
 
     def to_dict(self):
