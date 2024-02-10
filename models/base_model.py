e605530e851b39b6395a7fb1fdfb3f91a65152fc
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-10 03:19:32 -0800

    dict update

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,14 @@ Defines the BaseModels class with common attributes/methods.
 """
 
 
-from datetime import datetime
 import uuid
+from datetime import datetime
+ 
 
 
 class BaseModel:
     """
-    Class that defines all common methods/attributes for other classes
+    Base class for othr models.
     """
 
     def __init__(self, *args, **kwargs):
@@ -26,7 +27,9 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            self.name = "My_First_Model"
             self.register()
 
     def register(self):
@@ -54,13 +57,15 @@ class BaseModel:
         """
         Return a dictionary for all keys/values of __dict__ of the instance.
         """
-        return {**self.__dict__,
-                "__class__": self.__class__.__name__,
-                "created_at": self.created_at.isoformat(),
-                "updated_at": self.updated_at.isoformat()}
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
 
     def __str__(self):
         """
         Return a string representation fo the BaseModel instance.
         """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
