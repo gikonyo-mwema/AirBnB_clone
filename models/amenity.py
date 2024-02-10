1971687b6b20c2b23a3b3d3c9a5415942b761f4e
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-10 11:36:05 -0800

    all classes that inherit from BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the Amenity class.
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel.
+    """
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains the Review class.
+"""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """
+    Review class that inherits from BaseModel.
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
+
