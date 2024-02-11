da59fb68fb6cd94935a2dfa4bd3140ecb2d89b60
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-11 09:08:34 -0800

    pass to return

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,12 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
-# Additional classes
-from models.user import User
 from models.state import State
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+
 
 class FileStorage:
 
@@ -23,21 +22,25 @@ class FileStorage:
         """
         Returns the dictionary __objects
         """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """
         Sets in __objects the obj with key <obj class name>.id
         """
-        key = obj.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """
         Serializes __objects to the JSON file (path: __file_path)
         """
-        with open(self.__file_path, 'w') as f:
-            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+        obj_dict = {
+                k: v.to_dict()
+                for k, v in FileStorage.__objects.items()
+        }
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
 
     def reload(self):
         """
@@ -47,11 +50,11 @@ class FileStorage:
         no exception should be raised)
         """
         try:
-            with open(self.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, 'r') as f:
                 data = json.load(f)
                 for k, v in data.items():
                     cls_name = v['__class__']
                     instance = eval(cls_name)(**v)
-                    self.__objects[k] = instance
+                    FileStorage.__objects[k] = instance
         except FileNotFoundError:
-            pass
+            return
