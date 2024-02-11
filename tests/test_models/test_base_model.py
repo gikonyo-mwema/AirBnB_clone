e21d535029e3f9c40e3f4cfdc2cb50d38b031e71
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-10 18:29:42 -0800

    test shebange

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,62 +1,56 @@
+#!/usr/bin/python3
 """
 This module contains unit tests for the BaseModel class.
 """
 
 import unittest
+import datetime
 from models.base_model import BaseModel
-from datetime import datetime
+from models import storage
 
 class TestBaseModel(unittest.TestCase):
-    """
-    Defines tests for class BaseModel
-    """
+    """Tests for the BaseModel class."""
 
     def setUp(self):
-        """
-        Sets up the tests
-        """
+        """Set up the test case."""
         self.model1 = BaseModel()
-        self.model1.name = "ALX"
-        self.model1.my_number = 89
+        self.model2 = BaseModel()
 
-    def tearDown(self):
-        """
-        Ends the test
-        """
-        pass
+    def test_instantiation(self):
+        """Test the instantiation of a BaseModel instance."""
+        self.assertIsInstance(self.model1, BaseModel)
+        self.assertIsInstance(self.model1.id, str)
+        self.assertIsInstance(self.model1.created_at, datetime.datetime)
+        self.assertIsInstance(self.model1.updated_at, datetime.datetime)
+        self.assertIn(self.model1, storage.all().values())
+        self.assertNotEqual(self.model1.id, self.model2.id)
+        self.assertLess(self.model1.created_at, self.model2.created_at)
+        self.assertLess(self.model1.updated_at, self.model2.updated_at)
 
-    def test_attributes(self):
-        """
-        Tests the attributes of BaseModel
-        """
-        self.assertTrue(hasattr(self.model1, "name"))
-        self.assertTrue(hasattr(self.model1, "my_number"))
-        self.assertTrue(hasattr(self.model1, "created_at"))
-        self.assertTrue(hasattr(self.model1, "updated_at"))
-        self.assertTrue(hasattr(self.model1, "id"))
-
-    def test_save(self):
-        """
-        Tests the save method of BaseModel
-        """
-        old_updated_at = self.model1.updated_at
-        self.model1.save()
-        self.assertNotEqual(old_updated_at, self.model1.updated_at)
+    def test_str(self):
+        """Test the string representation of a BaseModel instance."""
+        self.assertEqual(str(self.model1), f"[BaseModel] ({self.model1.id}) {self.model1.__dict__}")
 
     def test_to_dict(self):
-        """
-        Tests the to_dict method of BaseModel
-        """
-        model1_dict = self.model1.to_dict()
-        self.assertEqual(self.model1.__class__.__name__, 'BaseModel')
-        self.assertEqual(model1_dict["__class__"], 'BaseModel')
-        self.assertEqual(model1_dict["name"], "ALX")
-        self.assertEqual(model1_dict["my_number"], 89)
-        self.assertTrue(isinstance(model1_dict["created_at"], str))
-        self.assertTrue(isinstance(model1_dict["updated_at"], str))
+        """Test the to_dict method of a BaseModel instance."""
+        model_dict = self.model1.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict['id'], self.model1.id)
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['created_at'], self.model1.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], self.model1.updated_at.isoformat())
+        self.assertNotEqual(model_dict, self.model1.__dict__)
 
+    def test_save(self):
+        """Test the save method of a BaseModel instance."""
+        old_updated_at = self.model1.updated_at
+        self.model1.save()
+        self.assertNotEqual(self.model1.updated_at, old_updated_at)
+
+    def tearDown(self):
+        """Tear down the test case."""
+        self.model1 = None
+        self.model2 = None
 
 if __name__ == '__main__':
     unittest.main()
-
-
