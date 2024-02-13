af96906d68cf0c3c4c2b89dae3f1dd41b70fa080
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-13 00:14:23 -0800

    filestorage change

--- models/base_model.py
+++ models/base_model.py
@@ -4,51 +4,37 @@ Module: base_model.py
 Defines the BaseModels class with common attributes/methods.
 """
 
-
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
     """
-    Base class for othr models.
+    Base class for other models.
     """
 
     def __init__(self, *args, **kwargs):
         """
-        Initialize Basemodel instance with unique id and current datetime.
+        Initialize BaseModel instance with unique id and current datetime.
         """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            self.add_to_storage()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = self.created_at
-                self.name = "My_First_Model"
-                self.add_to_storage()
+        else:
+            models.storage.new(self)
 
     def save(self):
         """
-        Update update_at with current datetime
+        Update updated_at with current datetime
         """
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
-
-    def add_to_storage(self):
-        """
-        Persist the instance in storage
-        """
-        from models import storage
-        storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """
@@ -62,7 +48,7 @@ class BaseModel:
 
     def __str__(self):
         """
-        Return a string representation fo the BaseModel instance.
+        Return a string representation of the BaseModel instance.
         """
         class_name = self.__class__.__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,8 +5,6 @@ Defines the FileStorage class for serializing and deserializing
 instances to/from a JSON file.
 """
 
-
-import os
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -22,12 +20,8 @@ class FileStorage:
     Handles serialization and deserialization of instances to/from JSON
     """
 
-    def __init__(self):
-        """
-        Initializes FileStorage instance with private attributes.
-        """
-        self.__file__path = "file.json"  # Path to the JSON file
-        self.__objects = {}  # Dictionary to store objects
+    __file_path = "file.json"  # Path to the JSON file
+    __objects = {}  # Dictionary to store objects
 
     def all(self):
         """
@@ -41,7 +35,7 @@ class FileStorage:
         """
         if not hasattr(obj, 'id'):
             raise ValueError("Object must have an 'id' attribute")
-        key = f"{obj.__class__.__name__}.{obj.id}"
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
@@ -51,11 +45,10 @@ class FileStorage:
         """
         serialized_objects = {}
         for key, obj in self.__objects.items():
-            # print(f"Object: {obj}, Type: {type(obj)}")
             if hasattr(obj, "to_dict"):  # Check if to_dict method exists
                 serialized_objects[key] = obj.to_dict()
 
-        with open(self.__file__path, "w") as json_file:
+        with open(self.__file_path, "w") as json_file:
             json.dump(serialized_objects, json_file)
 
     def reload(self):
@@ -63,25 +56,25 @@ class FileStorage:
         Deserializes the JSON file back into __objects.
         Only if the JSON file (__file_path) exists; otherwise, do nothing
         """
-        if not os.path.exists(self.__file__path):
-            return  # If the file doesn't exist, do nothing
-
-        with open(self.__file__path, "r") as json_file:
-            serialized_objects = json.load(json_file)
-            for key, serialized_obj in serialized_objects.items():
-                class_name, obj_id = key.split(".")
-                class_dict = {
-                        "BaseModel": BaseModel,
-                        "User": User,
-                        "State": State,
-                        "City": City,
-                        "Place": Place,
-                        "Amenity": Amenity,
-                        "Review": Review
-                }  # Add other classes
-                if class_name in class_dict:
-                    Class = class_dict[class_name]
-                    obj = Class(**serialized_obj)  # Create a new instance
-                    self.__objects[key] = obj
-                else:
-                    raise ValueError(f"Unknown class name: {class_name}")
+        try:
+            with open(self.__file_path, "r") as json_file:
+                serialized_objects = json.load(json_file)
+                for key, serialized_obj in serialized_objects.items():
+                    class_name, obj_id = key.split(".")
+                    class_dict = {
+                            "BaseModel": BaseModel,
+                            "User": User,
+                            "State": State,
+                            "City": City,
+                            "Place": Place,
+                            "Amenity": Amenity,
+                            "Review": Review
+                    }  # Add other classes
+                    if class_name in class_dict:
+                        Class = class_dict[class_name]
+                        obj = Class(**serialized_obj)  # Create a new instance
+                        self.__objects[key] = obj
+                    else:
+                        raise ValueError(f"Unknown class name: {class_name}")
+        except FileNotFoundError:
+            pass  # If the file doesn't exist, do nothing
